fd52bd63abf51ac68e28ae8571f7e34d931bbc4a
SavinPark <null>
2022-04-09 05:46:05 +0000

    헤더 푸터 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,25 @@
   <div id="app">
 	<!-- 자식 컴포넌트로부터 hello라는 이름으로 이벤트를 받음 -->
     <!-- HelloWorld :prop='hello' @hello='childCall'/ -->
+	<Header />
 	<Todolist />
+	<Footer />
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 import Todolist from './components/Todolist.vue'
-
+import Footer from './components/Footer.vue'
+	
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-	Todolist
+	Header,
+	Todolist,
+	Footer
   },
   data : function() {
 	  return {
